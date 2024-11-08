fa45b5ad704727bec5088fcc04e4fa83e91df77b
Chandhruj02 <chandhruj02@gmail.com>
2024-11-08 10:20:43 +0530

    Added Main Classes

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+		public static void main(String[] args){
+			System.out.println("------------------------------------------------------");
+			System.out.println("----------- Console Based - Banking System -----------");
+			System.out.println("------------------------------------------------------");
+			System.out.println("\nWelcome to Banking");
+			System.out.println("1. New User Registration");
+			System.out.println("2. Login with User ID");
+			System.out.println("3. Exit");
+			System.out.println("Enter your option: ");
+			Scanner in = new Scanner(System.in);
+			int option = in.nextInt();
+			switch(option) {
+				case 1:{
+						newUser nu = new newUser();
+						nu.generateNewUser();
+					break;
+				}
+				case 2:{
+					System.out.println("under Construction");
+					break;
+				}
+			}
+			in.close();
+		}
+
+	}
+
